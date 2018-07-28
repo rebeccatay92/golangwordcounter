7f1d62fc44157fb307debe2fd23d83389470ed1e
Rebecca Tay <rebeccatay92@gmail.com>
2018-07-28 21:09:37 +0800

    managed to read multiline text input in stdin

---
+++ README.md
@@ -0,0 +1,3 @@
+# Golang Word Counter
+
+Task: To make a servlet app in Golang that accepts as input a body of text (eg from a book), and returns the top ten most used words, along with how many times they occur in the text.
--- wordcounter.go
+++ wordcounter.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("Hello world")
+	fmt.Printf("Please enter, or paste input text body. To end input, hit Esc on a new line and then Enter.\n")
+
+	// var lines []string
+	var textbody string
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 1 {
+			// ascii hexadecimal for Esc
+			if line[0] == '\x1B' {
+				break
+			}
+		}
+		// lines = append(lines, line)
+		textbody += line
+	}
+
+	// if len(lines) > 0 {
+	// 	fmt.Println()
+	// 	fmt.Println("Result:")
+	// 	for _, line := range lines {
+	// 		fmt.Println("line", line)
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	if len(textbody) > 0 {
+		fmt.Println("Textbody:")
+		fmt.Println(textbody)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading std input:", err)
+	}
+
+	// words := strings.Fields(textbody)
+	// fmt.Println(words, len(words))
+
+	// refactor to use Replacer instead, to target many chars.
+	textbody = strings.ToLower(textbody)
+	textbody = strings.Replace(textbody, ".", "", -1)
+	textbody = strings.Replace(textbody, ",", "", -1)
+	fmt.Println("Cleaned:")
+	fmt.Println(textbody)
 }
