00d57666a54183f8dd2c7d63b99dc30a3ce1db79
Rebecca Tay <rebeccatay92@gmail.com>
2018-07-29 15:32:42 +0800

    esc key does not require newline to stop stdin. seperate new lines with a space

--- wordcounter.go
+++ wordcounter.go
@@ -8,48 +8,41 @@ import (
 )
 
 func main() {
-	fmt.Printf("Please enter, or paste input text body. To end input, hit Esc on a new line and then Enter.\n")
+	fmt.Printf("Please enter, or paste input text body. To end input, hit Esc and then Enter.\n")
 
-	// var lines []string
 	var textbody string
 	scanner := bufio.NewScanner(os.Stdin)
+
+	// for each line that is scanned
 	for scanner.Scan() {
 		line := scanner.Text()
-		if len(line) == 1 {
-			// ascii hexadecimal for Esc
-			if line[0] == '\x1B' {
-				break
-			}
-		}
-		// lines = append(lines, line)
-		textbody += line
-	}
 
-	// if len(lines) > 0 {
-	// 	fmt.Println()
-	// 	fmt.Println("Result:")
-	// 	for _, line := range lines {
-	// 		fmt.Println("line", line)
-	// 	}
-	// 	fmt.Println()
-	// }
+		// concatenate the strings, inserting a space to seperate lines. else 'abc', 'def' will become 'abcdef'
+		textbody += line + " "
+
+		// check if last char is Esc key, and stop scanning
+		if len(line) >= 1 && line[len(line)-1] == '\x1B' {
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading stdin:", err)
+	}
 
 	if len(textbody) > 0 {
 		fmt.Println("Textbody:")
 		fmt.Println(textbody)
+	} else {
+		fmt.Println("No text input detected")
 	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading std input:", err)
-	}
-
-	// words := strings.Fields(textbody)
-	// fmt.Println(words, len(words))
 
 	// refactor to use Replacer instead, to target many chars.
 	textbody = strings.ToLower(textbody)
-	textbody = strings.Replace(textbody, ".", "", -1)
-	textbody = strings.Replace(textbody, ",", "", -1)
+	textbody = strings.Replace(textbody, ".", " ", -1)
+	textbody = strings.Replace(textbody, ",", " ", -1)
 	fmt.Println("Cleaned:")
 	fmt.Println(textbody)
+
+	// words := strings.Fields(textbody)
+	// fmt.Println(words, len(words))
 }
