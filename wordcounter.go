c37e5646611bb3e8e0e53ee9360859d24b47c6f6
Rebecca Tay <rebeccatay92@gmail.com>
2018-07-30 17:02:25 +0800

    able to count and sort words. need to remove punctuation chars

--- wordcounter.go
+++ wordcounter.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"sort"
 	"strings"
 )
 
@@ -29,20 +31,58 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Error reading stdin:", err)
 	}
 
-	if len(textbody) > 0 {
-		fmt.Println("Textbody:")
-		fmt.Println(textbody)
-	} else {
-		fmt.Println("No text input detected")
+	/*
+		what counts as a word?
+		's 've 're 'll n't 'd 'm
+	*/
+	textbody = strings.ToLower(textbody)
+	textbody = strings.Replace(textbody, ".", "", -1)
+	textbody = strings.Replace(textbody, ",", "", -1)
+	textbody = strings.Replace(textbody, "\x1b", " ", -1)
+	// fmt.Println("Cleaned:")
+	// fmt.Println(textbody)
+
+	wordsSlice := strings.Fields(textbody)
+
+	if len(wordsSlice) < 1 {
+		fmt.Printf("There are no words found\n")
+		return
 	}
 
-	// refactor to use Replacer instead, to target many chars.
-	textbody = strings.ToLower(textbody)
-	textbody = strings.Replace(textbody, ".", " ", -1)
-	textbody = strings.Replace(textbody, ",", " ", -1)
-	fmt.Println("Cleaned:")
-	fmt.Println(textbody)
+	frequencyMap := make(map[string]int)
 
-	// words := strings.Fields(textbody)
-	// fmt.Println(words, len(words))
+	for _, word := range wordsSlice {
+		_, found := frequencyMap[word]
+		if found {
+			frequencyMap[word]++
+		} else if len(word) >= 1 {
+			frequencyMap[word] = 1
+		}
+	}
+
+	type Pair struct {
+		Word  string
+		Count int
+	}
+	var keyValueArr []Pair
+	for key, value := range frequencyMap {
+		keyValueArr = append(keyValueArr, Pair{key, value})
+	}
+
+	sort.Slice(keyValueArr, func(i, j int) bool {
+		return keyValueArr[i].Count > keyValueArr[j].Count
+	})
+
+	if len(keyValueArr) < 10 {
+		fmt.Println("There are less than 10 unique words")
+		for _, Pair := range keyValueArr {
+			fmt.Printf("%s appears %d times\n", Pair.Word, Pair.Count)
+		}
+	} else if len(keyValueArr) >= 10 {
+		fmt.Println("The 10 most frequent words are")
+		for i := 1; i <= 10; i++ {
+			fmt.Printf("%s appears %d times\n", keyValueArr[i].Word, keyValueArr[i].Count)
+		}
+	}
+
 }
